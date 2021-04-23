0ba7e2cf08aa795fc1eb1bb52458f8067e3fa978
andrefalvesp <andre_falvesp@hotmail.com>
2021-04-22 23:56:53 -0300

    post user

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ var path = require("path");
 var helmet = require('helmet');
 var rateLimit = require("express-rate-limit");
 
-var app = express();
+var bodyParser = require('body-parser')
+var app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 var server = http.createServer(app);
 
 const connectionString = 'postgresql://postgres:123456@localhost:5432/data-set-builder'
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || connectionString ,
+  connectionString: process.env.DATABASE_URL || connectionString,
   ssl: {
     rejectUnauthorized: false
   }
@@ -30,12 +37,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './public/form.html'));
 });
 
-app.get('/db', async (req, res) => {
+app.get('/user', async (req, res) => {
   try {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM user1');
-    const results = { 'results': (result) ? result.rows : null};
-    res.send(results );
+    const results = { 'results': (result) ? result.rows : null };
+    res.send(results);
     client.release();
   } catch (err) {
     console.error(err);
@@ -43,7 +50,22 @@ app.get('/db', async (req, res) => {
   }
 })
 
-var porta = process.env.PORT || 8080;
+app.post('/user', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const { idUser, name, email, date } = req.body;
+    const result = await client
+      .query('INSERT INTO user1(idUser,name, email, date) VALUES($1,$2,$3,$4)',
+        [idUser, name, email, date]
+      );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+var porta = process.env.PORT || 8081;
 server.listen(porta, function () {
   console.log("Server listening on port:" + porta);
 })
